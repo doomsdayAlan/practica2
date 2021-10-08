d3439cef8893f88d3aa27810e752cb48c0795a47
unknown <alanalexisaguilar@gmail.com>
2021-10-08 18:17:38 -0500

    parte2

--- descifrado.py
+++ descifrado.py
@@ -13,8 +13,24 @@ if tipoCifrado == 'CBC':
     print(cifrado.decode())
 elif tipoCifrado == 'ECB':
     key= b'holamundocomoest'
-    with open('cifrado_documento','rb') as c_files:
+    with open('cifrado_MODE_ECB','rb') as c_files:
         iv = c_files.read(16)
         descifrado= c_files.read()
     cipher= AES.new(key,AES.MODE_ECB,iv)
     cifrado = unpad(cipher.decrypt(descifrado),AES.block_size)
+elif tipoCifrado == 'CFB':
+    key= b'holamundocomoest'
+    with open('cifrado_MODE_CFB','rb') as c_files:
+        iv = c_files.read(16)
+        descifrado= c_files.read()
+    cipher= AES.new(key,AES.MODE_CFB,iv)
+    cifrado = unpad(cipher.decrypt(descifrado),AES.block_size)
+elif tipoCifrado == 'OFB':
+    key= b'holamundocomoest'
+    with open('cifrado_AES.MODE_OFB','rb') as c_files:
+        iv = c_files.read(16)
+        descifrado= c_files.read()
+    cipher= AES.new(key,AES.MODE_OFB,iv)
+    cifrado = unpad(cipher.decrypt(descifrado),AES.block_size)
+else:
+    print("no existe ese cifrado")
